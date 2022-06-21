6aecd39c14d696a4b2b781f634321b5f6ad4f6d7
Srijana Poudel <srijana.poudel@tuni.fi>
2022-06-21 17:38:57 +0300

    added html pages for snakeGame

---
+++ index.js
@@ -0,0 +1,18 @@
+// const container = document.getElementById("grid-container");
+// console.log(container);
+
+// function makeRows(rows, cols) {
+//   container.style.setProperty('--grid-rows', rows);
+//   container.style.setProperty('--grid-cols', cols);
+//   for (b = 0; b < (rows * cols); b++) {
+//     let box = document.createElement("div");
+//     // box.innerText = (b + 1);
+//     container.appendChild(box).className = "grid-item";
+//   };
+// };
+
+// makeRows(20, 20);
+
+function toSwitch(snakegame,info){
+    
+}
